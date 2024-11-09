b7714b4a0841f2924a896fbabdfeedafa5bd1954
Md. Shihabul Islam <166291764+Shihab23033@users.noreply.github.com>
2024-11-09 23:02:35 +0600

    Create Lab 2.java

---
+++ Lab 2.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class file_reading_advance {
+    public static void main(String[] args) {
+        try{
+            File file= new File("Input.txt");
+            Scanner scanf= new Scanner(file);
+            PrintWriter printf=new PrintWriter("Output 2.txt");
+            if(scanf.hasNextLine()){
+                String x =scanf.nextLine();
+                String[] s= x.split(",");
+                for(int i=0;i<s.length;i++){
+                    int n= Integer.parseInt(s[i]);
+                    //System.out.println(n);
+                    int ssum=(n*(n+1))/2;
+                    printf.print(ssum);
+                    if(i!=s.length-1)
+                    printf.print(", ");
+                }
+            }
+            scanf.close();
+            printf.close();
+            System.out.println("File written successfully");
+        }
+        catch(Exception e){
+            System.out.println("File not found");
+        }
+    }
+    
+}
