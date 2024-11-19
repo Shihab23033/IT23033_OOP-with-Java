0b220598cce1151f2a09a3eb247ae480297b0086
Shihab23033 <166291764+Shihab23033@users.noreply.github.com>
2024-11-19 16:26:23 +0600

    Update Ch2Ex8.java

--- Chapter_2/Ch2Ex8.java
+++ Chapter_2/Ch2Ex8.java
@@ -1,26 +1,22 @@
+import java.util.Scanner;
+
 public class Ch2Ex8 {
     public static void main(String[] args) {
         long totalMilliseconds = System.currentTimeMillis();
- 
+        Scanner scn= new Scanner(System.in);
+        String time=scn.nextLine();
+        int x =Integer.parseInt(time);
         // Obtain the total seconds since midnight, Jan 1, 1970
         long totalSeconds = totalMilliseconds / 1000;
- 
-        // Compute the current second in the minute in the hour
-        long currentSecond = totalSeconds % 60;
- 
-        // Obtain the total minutes
-        long totalMinutes = totalSeconds / 60;
- 
-        // Compute the current minute in the hour
-        long currentMinute = totalMinutes % 60;
-         // Obtain the total hours
+         long currentSecond = totalSeconds % 60;
+         long totalMinutes = totalSeconds / 60;
+         long currentMinute = totalMinutes % 60;
         long totalHours = totalMinutes / 60;
- 
-       // Compute the current hour
         long currentHour = totalHours % 24;
- 
+        currentHour+=x;
         // Display results
         System.out.println("Current time is " + currentHour + ":"
           + currentMinute + ":" + currentSecond + " GMT");
+          scn.close();
     }
 }
