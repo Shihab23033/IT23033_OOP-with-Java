92a99929fd9f74f2974002b076ee6fc838a7f88b
Shihab23033 <166291764+Shihab23033@users.noreply.github.com>
2024-12-17 09:28:13 +0600

    up

---
+++ Java Control Statements/CustomPrintCLass.java
@@ -0,0 +1,19 @@
+public class CustomPrintCLass {
+
+    public static void pr(String abc){
+        System.out.print(abc);
+    }
+    public static void prln(String abc){
+        System.out.println(abc);
+    }
+    public static void pr(int abc){
+        System.out.print(abc);
+    }
+    public static void pr(float abc){
+        System.out.print(abc);
+    }
+    public static void pr(double abc){
+        System.out.print(abc);
+    }
+    
+}
---
+++ Java Control Statements/MainCLass.java
@@ -0,0 +1,8 @@
+//package IT23033_OOP-with-Java.Java Control Statements;
+
+public class MainCLass {
+    public static void main(String[] args) {
+        CustomPrintCLass a =new CustomPrintCLass();
+                a.pr("adaf");
+    }
+}
